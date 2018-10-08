60ec987f6197219104fcf42f5c3cc0b8d1429d93
enesuzun2002 <4lukankalar@gmail.com>
2018-10-08 20:36:42 +0300

    Revert "mobicore: rootpa: define LOCAL_SDK_VERSION"
    This reverts commit b8e45e6f782922396d7306f4c9e0629301a88d97.

--- mobicore/rootpa/Code/Android/app/Android.mk
+++ mobicore/rootpa/Code/Android/app/Android.mk
@@ -13,7 +13,6 @@ LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_PACKAGE_NAME := RootPA
 LOCAL_MODULE_TAGS := eng optional
 LOCAL_CERTIFICATE := platform
-LOCAL_SDK_VERSION := current
 
 LOCAL_PROGUARD_FLAGS := -include $(LOCAL_PATH)/proguard-project.txt
 
